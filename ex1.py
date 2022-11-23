b652ebf9223465e6971084012d29f0f6acf53703
shyamkrishna <rskshyam@gmail.com>
2022-11-23 07:14:18 +0530

    adding ex1 and ex2 files as x=100 and y=200 from user as sanjana account

--- ex1.py
+++ ex1.py
@@ -3,3 +3,7 @@ This is first program for 2nd commit
 Printing first statement from ex1.py from developer1 branch
 
 Printing second  statement from ex1.py from developer1 branch
+
+************************************************************
+  X = 100                                              # Added X=100 from sanjana account for ex1 pgm
+************************************************************
